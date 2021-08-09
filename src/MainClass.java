d5f12812dc03594347c9dec0bc4e88d9c15ec3f6
Artem Dudarev <haskjas@gmail.com>
2021-08-09 15:14:32 +0300

    Add test data and test calls.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,21 @@
+import java.time.LocalDateTime;
+
 public class MainClass {
     public static void main(String[] args) {
-        ConsoleEmployeeStorageInteractor consoleEmployee = new ConsoleEmployeeStorageInteractor();
+        //ConsoleEmployeeStorageInteractor consoleEmployee = new ConsoleEmployeeStorageInteractor();
+        WorkLogger workLogger = null;
+        try {
+            workLogger = new WorkLogger("LogFile.txt");
+        }
+        catch (Exception e){
+            Writer.print(e.getMessage());
+        }
+
+        workLogger.writeToFile(1, true, LocalDateTime.now().minusMinutes(3));
+        workLogger.writeToFile(2, true, LocalDateTime.now().minusMinutes(2));
+        workLogger.writeToFile(2, false, LocalDateTime.now().plusHours(2));
+        workLogger.writeToFile(0, true, LocalDateTime.now().plusHours(3));
+        workLogger.writeToFile(1, false, LocalDateTime.now().plusHours(4));
+        workLogger.writeToFile(0, false, LocalDateTime.now().plusHours(5));
     }
 }
